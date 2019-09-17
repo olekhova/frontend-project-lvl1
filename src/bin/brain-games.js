2977ee9295bcdcea1e7cd01e0a01e3e0ad1832bb
Lyubov Olekhova <olekhova@gmail.com>
2019-09-17 09:50:00 +0300

    Added calc game, removed trash

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,6 +1,29 @@
 #!/usr/bin/env node
-import userName from '..';
+import { userName, userAnswer } from '..';
+import { question } from '../games/brain-calc';
 
-console.log('Welcome to the Brain Games!');
-const name = userName();
-console.log(`Hello, ${name}!`);
+const mainFunction = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('What is the result of the expression?');
+  const name = userName();
+  console.log(`Hello, ${name}!`);
+  let i = 0;
+  while (i < 3) {
+    const task = question();
+    console.log(task[0]);
+    const correctAnswer = task[1];
+    const answer = Number(userAnswer());
+    if (answer === correctAnswer) {
+      i += 1;
+      console.log('Correct!');
+    } else {
+      console.log(`${answer} is wrong answer ;(. Correct answer was  "${correctAnswer}". Let's try again, ${name}!`);
+      break;
+    }
+    if (i === 3) {
+      console.log(`Congratulations, ${name}!`);
+      break;
+    }
+  }
+};
+mainFunction();
---
+++ src/games/brain-calc.js
@@ -0,0 +1,36 @@
+const trueAnswer = (o, a, b) => {
+  if (o === '+') {
+    return a + b;
+  }
+  if (o === '-') {
+    return a - b;
+  }
+  return a * b;
+};
+export const rand = () => {
+  const arg1 = Math.floor(Math.random() * 100);
+  const arg2 = Math.floor(Math.random() * 100);
+
+  const arrayRandOperation = () => {
+    const arr = ['+', '-', '*'];
+    const randOp = Math.floor(Math.random() * arr.length);
+    return arr[randOp];
+  };
+  const op = arrayRandOperation();
+  const task = `${op}, ${arg1}, ${arg2}`;
+  return task;
+  // const correctAnswer = trueAnswer(op, arg1, arg2);
+};
+
+export const question = () => {
+  const arg1 = Math.floor(Math.random() * 100);
+  const arg2 = Math.floor(Math.random() * 100);
+
+  const arrayRandOperation = () => {
+    const arr = ['+', '-', '*'];
+    const randOp = Math.floor(Math.random() * arr.length);
+    return arr[randOp];
+  };
+  const op = arrayRandOperation();
+  return [`Question: ${arg1}${op}${arg2}`, trueAnswer(op, arg1, arg2)];
+};
